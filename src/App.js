175a57caabfd150ebfc10dcc32ed4aac7885fa7d
Mayank Gupta <mayankguptamg462@gmail.com>
2023-02-07 22:40:52 +0530

    sbbutton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import {Box} from '@mui/material'
+import React from 'react';
+import Sbbutton from './Sbbutton.js';
+import Sidebar from './Sidebar.js';
+
+import './App.css';
+
+
 function App() {
   return (
-    <div className="App">
-      <Box width={300}>
-        <h1>lets get started. hello wolrd</h1>
-        <p>The day had begun on a bright note. The sun finally peeked through the rain for the first time in a week, and the birds were sinf=ging in its warmth. There was no way to anticipate what was about to happen. It was a worst-case scenario and there was no way out of it.</p>
-      </Box>
+    
+      
+    <div id="main">
+  
+     
+      <Sbbutton/>
+     
     </div>
+    
+    
   );
 }
 
---
+++ src/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+function Sidebar() {
+  return (
+    <>
+    
+      
+      <button>hello by</button>
+      
+    </>
+    
+  );
+}
+
+export default Sidebar;
